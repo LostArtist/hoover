022e9f403c8b583caed9e579dc7a3d97b4faef2b
LostArtist <nikita.konovalov.brno@gmail.com>
2026-03-12 13:45:09 +0100

    added ProductNotFoundException

---
+++ src/main/java/com/hoover/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hoover.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
